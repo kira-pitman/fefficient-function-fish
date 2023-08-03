b441665b3fe59dc629741f281a3c6a3fe8078d55
Eden Pearson <pearsoneden@gmail.com>
2023-08-04 11:11:29 +1200

    2 tests passing for getFishingTrips

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -35,6 +35,36 @@ beforeAll(() => {
             },
           ],
         },
+        {
+          id: 2,
+          date: '2023-08-03',
+          location: 'idk man',
+          image:
+            'https://img.freepik.com/free-vector/happy-kid-sitting-chair-fishing_1308-121424.jpg?w=2000',
+          weather: 'clear',
+          fish: [
+            {
+              species: 'snapper',
+              length: 40,
+              bait: 'bonito',
+            },
+            {
+              species: 'trevally',
+              length: 50,
+              bait: 'bonito',
+            },
+            {
+              species: 'snapper',
+              length: 31,
+              bait: 'bonito',
+            },
+            {
+              species: 'snapper',
+              length: 33,
+              bait: 'bonito',
+            },
+          ],
+        },
       ],
     })
   )
@@ -50,36 +80,65 @@ test('getFishingTrips loads json from the right file', async () => {
   expect(data).toMatchInlineSnapshot(`
     {
       "fishingTrips": [
-             {
-               "date": "2023-08-01",
-               "fish": [
-                 {
-                   "bait": "bonito",
-                   "length": 40,
-                   "species": "snapper",
-                 },
-                 {
-                   "bait": "bonito",
-                   "length": 44,
-                   "species": "trevally",
-                 },
-                 {
-                   "bait": "bonito",
-                   "length": 31,
-                   "species": "snapper",
-                 },
-               ],
-               "id": 1,
-               "image": "https://img.freepik.com/free-vector/happy-kid-sitting-chair-fishing_1308-121424.jpg?w=2000",
-               "location": "Spirits Bay",
-               "weather": "overcast",
-             },
-           ],
-      }
+        {
+          "date": "2023-08-01",
+          "fish": [
+            {
+              "bait": "bonito",
+              "length": 40,
+              "species": "snapper",
+            },
+            {
+              "bait": "bonito",
+              "length": 44,
+              "species": "trevally",
+            },
+            {
+              "bait": "bonito",
+              "length": 31,
+              "species": "snapper",
+            },
+          ],
+          "id": 1,
+          "image": "https://img.freepik.com/free-vector/happy-kid-sitting-chair-fishing_1308-121424.jpg?w=2000",
+          "location": "Spirits Bay",
+          "weather": "overcast",
+        },
+        {
+          "date": "2023-08-03",
+          "fish": [
+            {
+              "bait": "bonito",
+              "length": 40,
+              "species": "snapper",
+            },
+            {
+              "bait": "bonito",
+              "length": 50,
+              "species": "trevally",
+            },
+            {
+              "bait": "bonito",
+              "length": 31,
+              "species": "snapper",
+            },
+            {
+              "bait": "bonito",
+              "length": 33,
+              "species": "snapper",
+            },
+          ],
+          "id": 2,
+          "image": "https://img.freepik.com/free-vector/happy-kid-sitting-chair-fishing_1308-121424.jpg?w=2000",
+          "location": "idk man",
+          "weather": "clear",
+        },
+      ],
+    }
   `)
 })
 
 test('getFishingTrips returns all trips', async () => {
   const data = await getFishingTrips()
-  expect(data.fishingTrips).toHaveLength(1)
+  expect(data.fishingTrips).toHaveLength(2)
 })
