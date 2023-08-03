6759fd5630512fead75baac09f0fbc81224a784d
Paul Guest <89233872+paul-c-guest@users.noreply.github.com>
2023-08-04 11:28:00 +1200

    update json from new fish form POST

--- server/route.js
+++ server/route.js
@@ -11,28 +11,28 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
-// router.post('/new', async (req, res) => {
-//   const trips = await getFishingTrips()
+router.post('/new', async (req, res) => {
+  const trips = await getFishingTrips()
 
-//   const id = trips.fishingTrips.length + 1
-//   const newTrip = {
-//     id,
-//     ...req.body,
-//   }
+  const id = trips.fishingTrips.length + 1
+  const newTrip = {
+    id,
+    ...req.body,
+  }
 
-//   trips.fishingTrips.push(newTrip)
+  trips.fishingTrips.push(newTrip)
 
-//   try {
-//     await fs.writeFile(
-//       Path.resolve('server/data/data.json'),
-//       JSON.stringify(trips, null, 2)
-//     )
-//   } catch (err) {
-//     console.error(err.message)
-//   }
+  try {
+    await fs.writeFile(
+      Path.resolve('server/data/data.json'),
+      JSON.stringify(trips, null, 2)
+    )
+  } catch (err) {
+    console.error(err.message)
+  }
 
-//   res.redirect(`/${newTrip.id}`)
-// })
+  res.redirect(`${newTrip.id}`)
+})
 
 router.get('/:id', async (req, res) => {
   try {
